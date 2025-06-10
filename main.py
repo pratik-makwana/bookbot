2794b8e0a1911ee9ef4fa74e43648b660c8a0e17
pratik-makwana <pm3038@gmail.com>
2025-06-10 15:19:20 +0530

    cerate function: get_chars_count()

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_chars_count
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -10,8 +10,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text =  get_book_text(book_path)
     num_words = get_num_words(text)
+    words_count = get_chars_count(text)
     print(f"{num_words} words found in the document")
-
+    print(words_count)
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,2 +1,11 @@
 def get_num_words(text):
     return len(text.split())
+
+
+def get_chars_count(content):
+    char_count = {}
+
+    for char in content:
+        char_count[char.lower()] = char_count.get(char.lower(), 0) + 1
+
+    return char_count
