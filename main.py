8ee6a6eba281fb14a3c0f96861f6c5336a56afdf
pratik-makwana <pm3038@gmail.com>
2025-06-10 15:42:37 +0530

    create functions: sort_on(), char_dict_to_sorted_list(), print_report()

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-from stats import get_num_words, get_chars_count
+from stats import get_num_words, get_chars_count, char_dict_to_sorted_list
 
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
     return file_contents
 
+def print_report(book_path, num_words, sorted_list):
+    print(
+        f"============ BOOKBOT ============\n Analyzing book found at {book_path}...\n"
+        f" ----------- Word Count ----------\n Found {num_words} total words\n"
+        f" --------- Character Count -------\n"
+    )  
+    for items in sorted_list:
+        dict_key = list(items.keys())[0]
+        dict_value = list(items.values())[0]
+        print(f"{dict_key}: {dict_value}")
+
+    print("============= END ===============")  
+
+
 
 def main():
     book_path = "books/frankenstein.txt"
     text =  get_book_text(book_path)
     num_words = get_num_words(text)
-    words_count = get_chars_count(text)
-    print(f"{num_words} words found in the document")
-    print(words_count)
+    chars_count = get_chars_count(text)
+    char_sorted_list = char_dict_to_sorted_list(chars_count)
+    print_report(book_path=book_path, num_words=num_words, sorted_list=char_sorted_list)
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -9,3 +9,18 @@ def get_chars_count(content):
         char_count[char.lower()] = char_count.get(char.lower(), 0) + 1
 
     return char_count
+
+
+def sort_on(dictionary):
+    
+    for (key, value) in dictionary.items():
+        if isinstance(value, (int, float)):
+            return value
+    return -float('inf')
+
+
+def char_dict_to_sorted_list(d):
+
+    sorted_list = [{k: v} for k, v in d.items() if k.isalpha()]
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
