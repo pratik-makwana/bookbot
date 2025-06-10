3e464e300729f2faf6e4117daa8a9f4f9108d579
pratik-makwana <pm3038@gmail.com>
2025-06-10 15:09:13 +0530

    create stats.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+from stats import get_num_words
+
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
     return file_contents
-
-def get_num_words(text):
-    return len(text.split())
 
 
 def main():
